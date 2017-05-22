86a1e0a0877edc8c714ff197f9c7296296820d79
Matthias Ferch <matthias.ferch@gmail.com>
2017-05-22 11:28:03 +0200

    Added annotations.

--- Wunderwerk/Containers/List.ts
+++ Wunderwerk/Containers/List.ts
@@ -28,11 +28,20 @@ module Wunderwerk {
 
     }
 
+    /**
+    * A dynamic array implementation of the List interface.
+    */
+
     export class ArrayList<T> implements List<T> {
 
         private m_values: T[];
 
         private m_updated: boolean;
+
+        /**
+         * Constructor.
+         * @param {null | T[]} values: initial values or null.
+         */
 
         constructor(values: null | T[] = null) {
             if (values) {
@@ -44,17 +53,37 @@ module Wunderwerk {
             this.m_updated = true;
         }
 
+        /**
+         * Creates a copy of the array list.
+         * @return {ArrayList<T>} A copy of the array list.
+         */
+
         copy(): ArrayList<T> {
             return new ArrayList<T>(this.m_values);
         }
+
+        /**
+         * Returns the number of elements in the array list.
+         * @return {number} The number of elements of the array list.
+         */
 
         get count(): number {
             return this.m_values.length;
         }
 
+        /**
+         * Checks whether the array list is empty.
+         * @return {boolean} True if the number of elements in the array list is zero, false otherwise.
+         */
+
         get empty(): boolean {
             return (this.m_values.length === 0);
         }
+
+        /**
+         * Checks whether any elements have been added, replaced, removed or swapped since the last check.
+         * @return {boolean} True if the array list has been updated since the last check, false otherwise.
+         */
 
         get updated(): boolean {
             let updated = this.m_updated;
@@ -64,6 +93,11 @@ module Wunderwerk {
             return updated;
         }
 
+        /**
+         * Returns a reference to the first element in the array list (or null if the list is empty).
+         * @return {null | T} The first element (or null).
+         */
+
         get first(): null | T {
             if (this.m_values.length === 0) {
                 return null;
@@ -72,6 +106,11 @@ module Wunderwerk {
             return this.m_values[0];
         }
 
+        /**
+         * Returns a reference to the last element in the array list (or null if the list is empty).
+         * @return {null | T} The last element (or null).
+         */
+
         get last(): null | T {
             if (this.m_values.length === 0) {
                 return null;
@@ -79,6 +118,11 @@ module Wunderwerk {
 
             return this.m_values[this.m_values.length - 1];
         }
+
+        /**
+         * Removes all elements from the array list.
+         * @return {boolean} True if any elements have been removed, false otherwise.
+         */
 
         clear(): boolean {
             if (this.m_values.length === 0) {
@@ -92,6 +136,12 @@ module Wunderwerk {
             return true;
         }
 
+        /**
+         * Returns a reference to the element at the given index (or null if the index is out of range).
+         * @param {number} index: The requested index. Must be zero or positive.
+         * @return {null | T} The element at the given index or null.
+         */
+
         at(index: number): null | T {
             if (index >= this.m_values.length) {
                 return null;
@@ -100,9 +150,21 @@ module Wunderwerk {
             return this.m_values[index];
         }
 
+        /**
+         * Checks whether the given value is an element of the array list.
+         * @param {T} value: The requested value.
+         * @return {boolean} True if the value is an element of the list, false otherwise.
+         */
+
         has(value: T): boolean {
             return this.index(value) !== null;
         }
+
+        /**
+         * Returns the array index of the given value (or null if the value is not an element of the list).
+         * @param {T} value: The requested value.
+         * @return {null | number} The array index of the given value or null.
+         */
 
         index(value: T): null | number {
             for (let i = 0, len = this.m_values.length; i < len; i++) {
@@ -113,6 +175,14 @@ module Wunderwerk {
 
             return null;
         }
+
+        /**
+         * Adds a value to the list array and returns the array index of the new element
+         * (or null if the array already has an element with the same value and duplicate entries are not allowed).
+         * @param {T} value: The value to be added.
+         * @param {boolean} allowDuplicates: Whether of not to allow duplicate entries.
+         * @return {null | number} The array index of the new element or null.
+         */
 
         add(value: T, allowDuplicates = true): null | number {
             if (!allowDuplicates) {
@@ -128,6 +198,13 @@ module Wunderwerk {
             return this.m_values.length - 1;
         }
 
+        /**
+         * Replaces the element at the given index with the given value and returns the replaced value.
+         * @param {number} index: The index of the element that is to be replaced.
+         * @param {T} value: The new value replacing the old element.
+         * @return {null | number} The replaced value or null.
+         */
+
         replace(index: number, value: T): null | T {
             let replacedValue = this.m_values[index];
 
@@ -137,6 +214,12 @@ module Wunderwerk {
 
             return replacedValue;
         }
+
+        /**
+         * Removes the element with the given value from the array list.
+         * @param {T} value: The value to be removed from the list.
+         * @return {null | number} True if the value has been found and removed, false otherwise.
+         */
 
         remove(value: T): boolean {
             for (let i = this.m_values.length - 1; i >= 0; i--) {
@@ -152,6 +235,12 @@ module Wunderwerk {
             return false;
         }
 
+        /**
+         * Removes the element at the given index from the array list and returns the removed value.
+         * @param {number} index: The array index to be removed from the list.
+         * @return {null | number} The removed value or null.
+         */
+
         removeAt(index: number): null | T {
             if (index >= this.m_values.length) {
                 return null;
@@ -165,6 +254,13 @@ module Wunderwerk {
 
             return removedValue;
         }
+
+        /**
+         * Swaps the array elements with the two given values.
+         * @param {T} first: The first value.
+         * @param {T} second: The second value.
+         * @return {boolean} True if the elements have been successfully swapped, false otherwise.
+         */
 
         swap(first: T, second: T): boolean {
             let firstIndex = this.index(first),
@@ -187,6 +283,13 @@ module Wunderwerk {
             return swapped;
         }
 
+        /**
+         * Swaps the array elements at the two given indices.
+         * @param {number} firstIndex: The first array index.
+         * @param {number} secondIndex: The second array index.
+         * @return {boolean} True if the elements have been successfully swapped, false otherwise.
+         */
+
         swapAt(firstIndex: number, secondIndex: number): boolean {
             if (firstIndex >= this.m_values.length) {
                 return false;
@@ -206,7 +309,12 @@ module Wunderwerk {
             return true;
         }
 
-        each(callback: (index: number, value: T) => void): void {
+        /**
+         * Iterates over the elements in the array list and executes the given callback function on each of them.
+         * @param {(index: number, value: T) => void} callback: The callback function to be executed on each element of the list.
+         */
+
+        each(callback: (index: number, value: T) => void) {
             for (let i = 0, len = this.m_values.length; i < len; i++) {
                 callback(i, this.m_values[i]);
             }
